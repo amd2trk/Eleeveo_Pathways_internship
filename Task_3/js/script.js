b39d49a0254b94b162959aea4e085ccd14e754ab
amd2trk <ahmed.tarek.20032013@gmail.com>
2025-09-04 20:21:44 +0300

    Task 3

---
+++ Task_3/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+            const sections = document.querySelectorAll(".animated-section");
+
+            const observerOptions = {
+                root: null, // relative to the viewport
+                rootMargin: "0px",
+                threshold: 0.1 // 10% of the element must be visible
+            };
+
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("is-visible");
+                        observer.unobserve(entry.target); // Stop observing once it's visible
+                    }
+                });
+            }, observerOptions);
+
+            sections.forEach(section => {
+                observer.observe(section);
+            });
+        });
